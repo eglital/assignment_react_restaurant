15505d972c4c66c69a9a66bad19502b9a4bb0a7c
Renzo Tomlinson <rttomlinson@gmail.com>
2017-05-01 22:35:28 +0000

    Add styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-default">
+    <nav className="navbar navbar-default" id="navbar">
       <div className="container-fluid">
         <div className="navbar-header">
           <button
